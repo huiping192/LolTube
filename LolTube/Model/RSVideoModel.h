982de71c71fb5e7b12d00f7bc59a6127911ef852
huiping192 <huiping192@gmail.com>
2014-09-02 01:51:32 +0900

    implement recive youtube video list

---
+++ LolTube/Model/RSVideoModel.h
@@ -0,0 +1,8 @@
+//
+// Created by 郭 輝平 on 9/2/14.
+// Copyright (c) 2014 Huiping Guo. All rights reserved.
+//
+
+
+@interface RSVideoModel : NSObject
+@end
---
+++ LolTube/Model/RSVideoModel.m
@@ -0,0 +1,12 @@
+//
+// Created by 郭 輝平 on 9/2/14.
+// Copyright (c) 2014 Huiping Guo. All rights reserved.
+//
+
+#import "RSVideoModel.h"
+
+
+@implementation RSVideoModel {
+
+}
+@end
