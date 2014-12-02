9d08aeb25290ee91aba2cfa03a1452e5b64b50bf
huiping192 <huiping192@gmail.com>
2014-12-03 01:06:36 +0900

    change video list cell margin to 10 when using ipad

--- LolTube/ViewController/RSVideoListCollectionViewLayout.m
+++ LolTube/ViewController/RSVideoListCollectionViewLayout.m
@@ -8,21 +8,35 @@
 static CGFloat const kCellMinWidth = 280.0f;
 static CGFloat const kCellRatio = 180.0f / 320.0f;
 
+
+static CGFloat const kCellMarginPhone = 5.0f;
+static CGFloat const kCellMarginPad = 10.0f;
+
 @implementation RSVideoListCollectionViewLayout {
 
+}
+
+- (void)prepareLayout {
+    [super prepareLayout];
+
+    CGFloat cellMargin = [self p_cellMargin];
+    [self setSectionInset:UIEdgeInsetsMake(cellMargin, cellMargin, cellMargin, cellMargin)];
 }
 
 - (CGSize)itemSize {
     CGFloat collectionWidth = self.collectionView.frame.size.width;
     int cellCount = (int) (collectionWidth / kCellMinWidth);
 
-    CGFloat cellWidth = (collectionWidth - 5 * (cellCount + 1)) / cellCount;
-
+    CGFloat cellWidth = (collectionWidth - [self p_cellMargin] * (cellCount + 1)) / cellCount;
     return CGSizeMake(cellWidth, cellWidth * kCellRatio);
 }
 
-- (UIEdgeInsets)sectionInset {
-    return UIEdgeInsetsMake(5, 5, 5, 5);
+- (CGFloat)minimumLineSpacing {
+    return [self p_cellMargin];
+}
+
+- (CGFloat)p_cellMargin {
+    return [[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone ? kCellMarginPhone : kCellMarginPad;
 }
 
 
