bd663f2557b6b7cdc60247ee5f9dcf60245fa949
Huiping Guo <huiping192@gmail.com>
2014-12-20 20:40:35 +0900

    cache NSDateFormatter

--- LolTube/Common/Category/NSDate+RSFormatter.m
+++ LolTube/Common/Category/NSDate+RSFormatter.m
@@ -6,19 +6,22 @@
 #import "NSDate+RSFormatter.h"
 #import "RSStringUtil.h"
 
-
 @implementation NSDate (RSFormatter)
 
 + (NSDate *)dateFromISO8601String:(NSString *)iso8601String {
+    static NSDateFormatter *formatterCache = nil;
     if([RSStringUtil isEmptyString:iso8601String]){
         return nil;
     }
-    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
-    [formatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss.SSSZ"];
-    NSLocale *posix = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"];
-    [formatter setLocale:posix];
+    if(!formatterCache){
+        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+        [formatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss.SSSZ"];
+        NSLocale *posix = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"];
+        [formatter setLocale:posix];
+        formatterCache = formatter;
+    }
 
-    return [formatter dateFromString:iso8601String];
+    return [formatterCache dateFromString:iso8601String];
 }
 
 
