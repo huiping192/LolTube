924bd97808659e54cdcaef061887257d0b54a0d7
huiping192 <huiping192@gmail.com>
2015-04-29 18:50:38 +0900

    update cocoapods to 0.36.3 and add quick test framework

--- Pods/Pods-Google-AdMob-Ads-SDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Google_AdMob_Ads_SDK : NSObject
-@end
-@implementation PodsDummy_Pods_Google_AdMob_Ads_SDK
-@end
--- Pods/Pods-VideoWidget-Google-Mobile-Ads-SDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_VideoWidget_Google_Mobile_Ads_SDK : NSObject
-@end
-@implementation PodsDummy_Pods_VideoWidget_Google_Mobile_Ads_SDK
-@end
