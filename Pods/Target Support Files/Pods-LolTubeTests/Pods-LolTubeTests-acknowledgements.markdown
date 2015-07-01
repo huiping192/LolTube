ef5b3b4de72316fdb978d6f3bdc622554d256beb
huiping192 <huiping192@gmail.com>
2015-07-01 23:23:17 +0900

    remove admob lib

--- Pods/Target Support Files/Pods-LolTubeTests/Pods-LolTubeTests-acknowledgements.markdown
+++ Pods/Target Support Files/Pods-LolTubeTests/Pods-LolTubeTests-acknowledgements.markdown
@@ -24,10 +24,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 
 
-## Google-Mobile-Ads-SDK
-
-Copyright 2011 Google Inc. All Rights Reserved.
-
 ## GoogleAnalytics-iOS-SDK
 
 Copyright 2009 - 2014 Google, Inc. All rights reserved.
--- Pods/Target Support Files/Pods/Pods-environment.h
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -48,12 +48,6 @@
 #define COCOAPODS_VERSION_MINOR_AFNetworking_UIKit 4
 #define COCOAPODS_VERSION_PATCH_AFNetworking_UIKit 1
 
-// Google-Mobile-Ads-SDK
-#define COCOAPODS_POD_AVAILABLE_Google_Mobile_Ads_SDK
-#define COCOAPODS_VERSION_MAJOR_Google_Mobile_Ads_SDK 7
-#define COCOAPODS_VERSION_MINOR_Google_Mobile_Ads_SDK 0
-#define COCOAPODS_VERSION_PATCH_Google_Mobile_Ads_SDK 0
-
 // GoogleAnalytics-iOS-SDK
 #define COCOAPODS_POD_AVAILABLE_GoogleAnalytics_iOS_SDK
 #define COCOAPODS_VERSION_MAJOR_GoogleAnalytics_iOS_SDK 3
