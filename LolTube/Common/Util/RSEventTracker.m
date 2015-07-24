8668470de1126f3e2d8921bee3a4b04d744c62b3
huiping192 <huiping192@gmail.com>
2015-07-24 23:09:07 +0900

    google analytics do not support cocoapods frame,temporary comment out RSEventTracker class

--- LolTube/Common/Util/RSEventTracker.m
+++ LolTube/Common/Util/RSEventTracker.m
@@ -4,8 +4,8 @@
 //
 
 #import "RSEventTracker.h"
-#import <GoogleAnalytics-iOS-SDK/GAI.h>
-#import <GoogleAnalytics-iOS-SDK/GAIDictionaryBuilder.h>
+//#import <GoogleAnalytics-iOS-SDK/GAI.h>
+//#import <GoogleAnalytics-iOS-SDK>
 
 static NSString *const kReleaseTrackingId = @"UA-56633617-1";
 static NSString *const kDevelopmentTrackingId = @"UA-56633617-2";
@@ -15,30 +15,30 @@ static NSString *const kDevelopmentTrackingId = @"UA-56633617-2";
 }
 
 + (void)configure {
-    [GAI sharedInstance].trackUncaughtExceptions = YES;
-#if DEBUG
-    [GAI sharedInstance].dispatchInterval = 20;
-    [[[GAI sharedInstance] logger] setLogLevel:kGAILogLevelVerbose];
-    [[GAI sharedInstance] trackerWithTrackingId:kDevelopmentTrackingId];
-#else
-    [[GAI sharedInstance] trackerWithTrackingId:kReleaseTrackingId];
-#endif
+//    [GAI sharedInstance].trackUncaughtExceptions = YES;
+//#if DEBUG
+//    [GAI sharedInstance].dispatchInterval = 20;
+//    [[[GAI sharedInstance] logger] setLogLevel:kGAILogLevelVerbose];
+//    [[GAI sharedInstance] trackerWithTrackingId:kDevelopmentTrackingId];
+//#else
+//    [[GAI sharedInstance] trackerWithTrackingId:kReleaseTrackingId];
+//#endif
 }
 
 + (void)trackVideoDetailShareWithVideoId:(NSString *)videoId {
-    [[[GAI sharedInstance] defaultTracker] send:[[GAIDictionaryBuilder createEventWithCategory:@"video_detail" action:@"video_share" label:videoId value:nil] build]];
+//    [[[GAI sharedInstance] defaultTracker] send:[[GAIDictionaryBuilder createEventWithCategory:@"video_detail" action:@"video_share" label:videoId value:nil] build]];
 }
 
 + (void)trackVideoDetailPlayWithVideoId:(NSString *)videoId {
-    [[[GAI sharedInstance] defaultTracker] send:[[GAIDictionaryBuilder createEventWithCategory:@"video_detail" action:@"video_play" label:videoId value:nil] build]];
+//    [[[GAI sharedInstance] defaultTracker] send:[[GAIDictionaryBuilder createEventWithCategory:@"video_detail" action:@"video_play" label:videoId value:nil] build]];
 }
 
 + (void)trackVideoListSearchWithSearchText:(NSString *)searchText {
-    [[[GAI sharedInstance] defaultTracker] send:[[GAIDictionaryBuilder createEventWithCategory:@"video_list" action:@"video_search" label:searchText value:nil] build]];
+//    [[[GAI sharedInstance] defaultTracker] send:[[GAIDictionaryBuilder createEventWithCategory:@"video_list" action:@"video_search" label:searchText value:nil] build]];
 }
 
 + (void)trackChannelListDeleteWithChannelId:(NSString *)channelId {
-    [[[GAI sharedInstance] defaultTracker] send:[[GAIDictionaryBuilder createEventWithCategory:@"channel_list" action:@"channel_delete" label:channelId value:nil] build]];
+//    [[[GAI sharedInstance] defaultTracker] send:[[GAIDictionaryBuilder createEventWithCategory:@"channel_list" action:@"channel_delete" label:channelId value:nil] build]];
 }
 
 @end
