8b1929a3436d55df32ef41150c22692ed57dae47
huiping192 <huiping192@gmail.com>
2015-09-23 13:57:41 +0900

    add google tacking

--- LolTube/Common/Util/EventTracker.swift
+++ LolTube/Common/Util/EventTracker.swift
@@ -7,34 +7,66 @@ class EventTracker: NSObject {
             contentType: String(viewType),
             contentId: viewId,
             customAttributes: nil)
+        
+       EventTracker.trackGoogleAnalyticsScreen(screenName: viewName)
     }
     
     static func trackViewDetailShare(activityType activityType:String,videoId:String) {
+        let contentType = "Video"
         Answers.logShareWithMethod(activityType,
             contentName: videoId,
-            contentType: "Video",
+            contentType: contentType,
             contentId: videoId,
             customAttributes: nil)
+        
+        EventTracker.trackGoogleAnalyticsEvent(category: contentType, action: "Share", label: videoId)
     }
     
     static func trackVideoDetailPlay(videoId:String) {
+        let category = "Video"
+        let action = "Play"
         Answers.logCustomEventWithName("Play Video",
             customAttributes: ["videoId":videoId])
+        
+        EventTracker.trackGoogleAnalyticsEvent(category: category, action: action, label: videoId)
     }
     
     static func trackSearch(searchText:String){
+        let category = "Search"
+        let action = "Search"
+
         Answers.logSearchWithQuery(searchText,
             customAttributes: nil)
+        
+        EventTracker.trackGoogleAnalyticsEvent(category: category, action: action, label: searchText)
     }
     
     static func trackAddChannel(channelTitle channelTitle:String,channelId:String){
         Answers.logCustomEventWithName("Add Channel",
             customAttributes: ["channelTitle":channelTitle,"channelId":channelId])
+        
+        EventTracker.trackGoogleAnalyticsEvent(category: "Channel", action: "Add", label: channelTitle)
     }
     
     static func trackDeleteChannel(channelTitle channelTitle:String,channelId:String){
         Answers.logCustomEventWithName("Delete Channel",
             customAttributes: ["channelTitle":channelTitle,"channelId":channelId])
+        
+        EventTracker.trackGoogleAnalyticsEvent(category: "Channel", action: "Delete", label: channelTitle)
     }
+    
+    private static func trackGoogleAnalyticsScreen(screenName screenName:String){
+        let tracker = GAI.sharedInstance().defaultTracker
+        tracker.set(kGAIScreenName, value: screenName)
+        let parameters = GAIDictionaryBuilder.createScreenView().build() as [NSObject : AnyObject]
+        tracker.send(parameters)
+    }
+    
+    private static func trackGoogleAnalyticsEvent(category category:String,action:String,label:String? = nil) {
+        let tracker = GAI.sharedInstance().defaultTracker
+        let parameters =  GAIDictionaryBuilder.createEventWithCategory(category, action: action, label: label, value: nil).build() as [NSObject : AnyObject]
+        tracker.send(parameters)
+    }
+
     
 }
